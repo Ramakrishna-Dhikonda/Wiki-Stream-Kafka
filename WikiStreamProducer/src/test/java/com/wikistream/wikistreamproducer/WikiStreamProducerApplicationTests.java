c09f1ef281ed71046f69238acd18a1e47f2cb2f7
Ramakrishna-Dhikonda <Ramkrishna.dhikonda@gmail.com>
2026-02-23 22:46:31 +0530

    Update as per feedback

--- WikiStreamProducer/src/test/java/com/wikistream/wikistreamproducer/WikiStreamProducerApplicationTests.java
+++ WikiStreamProducer/src/test/java/com/wikistream/wikistreamproducer/WikiStreamProducerApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class WikiStreamProducerApplicationTests {
 
     @Test
-    void contextLoader() {
+    void contextLoaderImprovedVersion() {
     }
 
 }
